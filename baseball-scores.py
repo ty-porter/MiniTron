cb2bfbece017c98f1f8ddd7241c68bd2a1a7b368
unknown <tyler.b.porter@gmail.com>
2019-06-18 19:57:20 -0500

    Added losing team and pitcher stats to display

--- baseball-scores.py
+++ baseball-scores.py
@@ -25,16 +25,35 @@ class Scoreboard(SampleBase):
                 font = graphics.Font()
                 font.LoadFont("../../../fonts/4x6.bdf")
                 textColor = graphics.Color(255, 255, 255)
-                pos = offscreen_canvas.width
-                my_text = self.data['games'][0]['game']['winning_team']['name']              
+                x_pos = 0
+                y_pos = 5
+
+                # Teams & Scores
+                winning_team = self.data['games'][0]['game']['winning_team']['name']
+                winning_team_runs = self.data['games'][0]['game']['winning_team']['runs']
+                losing_team = self.data['games'][0]['game']['losing_team']['name']
+                losing_team_runs = self.data['games'][0]['game']['losing_team']['runs']
+
+                # Pitchers
+                winning_pitcher = self.data['games'][0]['game']['winning_team']['winning_pitcher']['name']
+                winning_pitcher_record_object = self.data['games'][0]['game']['winning_team']['winning_pitcher']['record']
+                winning_pitcher_record = str(winning_pitcher_record_object['wins']) + ' - ' + str(winning_pitcher_record_object['losses'])
+
+                losing_pitcher = self.data['games'][0]['game']['losing_team']['losing_pitcher']['name']
+                losing_pitcher_record_object = self.data['games'][0]['game']['losing_team']['losing_pitcher']['record']
+                losing_pitcher_record = str(losing_pitcher_record_object['wins']) + ' - ' + str(losing_pitcher_record_object['losses'])
 
                 while True:
                         offscreen_canvas.Clear()
-                        len = graphics.DrawText(offscreen_canvas, font, pos, 35, textColor, my_text)
-                        pos -= 1
-                        if (pos + len < 0):
-                                        pos = offscreen_canvas.width
+                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos, textColor, winning_team)                                              
+                        graphics.DrawText(offscreen_canvas, font, ( offscreen_canvas.width - ( len(str(winning_team_runs)) * 4 ) ), y_pos, textColor, str(winning_team_runs))
+                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 7, textColor, losing_team)                                              
+                        graphics.DrawText(offscreen_canvas, font, ( offscreen_canvas.width - ( len(str(winning_team_runs)) * 4 ) ), y_pos + 7, textColor, str(losing_team_runs))
 
+                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 21, textColor, "Pitching:")
+                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 28, textColor, "W " + winning_pitcher + winning_pitcher_record)
+                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 35, textColor, "L " + losing_pitcher + losing_pitcher_record)
+                        
                         time.sleep(0.05)
                         offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 

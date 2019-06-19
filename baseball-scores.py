c726ccd125b327665f1715a68699b1e698a0d69f
unknown <tyler.b.porter@gmail.com>
2019-06-18 19:30:30 -0500

    Displays data correctly, imported fonts

--- baseball-scores.py
+++ baseball-scores.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python
+from samplebase import SampleBase
 import requests
 import datetime
+from rgbmatrix import graphics
+import time
 
-class Scoreboard():
-	def __init__(self):
-		self.getData()
+class Scoreboard(SampleBase):
+        def __init__(self, *args, **kwargs):
+                super(Scoreboard, self).__init__(*args, **kwargs)
+                self.getData()
 
-	def getData(self):
-		date = datetime.datetime.now()
-		DATE = str(date.month) + "/" + str(date.day - 1) + "/" + str(date.year)
-		URL = "https://mlb-api-server.herokuapp.com/api/games/" + DATE
+        # Get yesterday's date, and make a GET request to server, then parse results.
+        def getData(self):
+                date = datetime.datetime.now()
+                DATE = str(date.month) + "/" + str(date.day - 1) + "/" + str(date.year)
+                URL = "https://mlb-api-server.herokuapp.com/api/games/" + DATE
 
-		r = requests.get(URL)
-		data = r.json()
+                r = requests.get(URL)
+                self.data = r.json()
+                
 
-		print(data)
+        def run(self):
+                offscreen_canvas = self.matrix.CreateFrameCanvas()
+                font = graphics.Font()
+                font.LoadFont("../../../fonts/4x6.bdf")
+                textColor = graphics.Color(255, 255, 255)
+                pos = offscreen_canvas.width
+                my_text = self.data['games'][0]['game']['winning_team']['name']              
+
+                while True:
+                        offscreen_canvas.Clear()
+                        len = graphics.DrawText(offscreen_canvas, font, pos, 35, textColor, my_text)
+                        pos -= 1
+                        if (pos + len < 0):
+                                        pos = offscreen_canvas.width
+
+                        time.sleep(0.05)
+                        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 # Main function
 if __name__ == "__main__":
     scoreboard = Scoreboard()
+    if (not scoreboard.process()):
+        scoreboard.print_help()
 	

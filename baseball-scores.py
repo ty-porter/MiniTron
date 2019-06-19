42c6c785e0f334ae255e87a387ef5d0fe76d42d1
unknown <tyler.b.porter@gmail.com>
2019-06-18 23:58:02 -0500

    Scoreboard scrolls through game details correctly

--- baseball-scores.py
+++ baseball-scores.py
@@ -4,62 +4,105 @@ import requests
 import datetime
 from rgbmatrix import graphics
 import time
+from teams import teams
 
 class Scoreboard(SampleBase):
-        def __init__(self, *args, **kwargs):
-                super(Scoreboard, self).__init__(*args, **kwargs)
-                self.getData()
+  def __init__(self, *args, **kwargs):
+    super(Scoreboard, self).__init__(*args, **kwargs)
+    self.getData()
 
-        # Get yesterday's date, and make a GET request to server, then parse results.
-        def getData(self):
-                date = datetime.datetime.now()
-                DATE = str(date.month) + "/" + str(date.day - 1) + "/" + str(date.year)
-                URL = "https://mlb-api-server.herokuapp.com/api/games/" + DATE
+  # Get yesterday's date, and make a GET request to server, then parse results.
+  def getData(self):
+    date = datetime.datetime.now()
+    DATE = str(date.month) + "/" + str(date.day - 1) + "/" + str(date.year)
+    URL = "https://mlb-api-server.herokuapp.com/api/games/" + DATE
 
-                r = requests.get(URL)
-                self.data = r.json()
-                
+    r = requests.get(URL)
+    self.data = r.json()
+                                  
 
-        def run(self):
-                offscreen_canvas = self.matrix.CreateFrameCanvas()
-                font = graphics.Font()
-                font.LoadFont("../../../fonts/4x6.bdf")
-                textColor = graphics.Color(255, 255, 255)
-                x_pos = 0
-                y_pos = 5
+  def run(self):
+    offscreen_canvas = self.matrix.CreateFrameCanvas()
+    font = graphics.Font()
+    font.LoadFont("../../../fonts/4x6.bdf")
+    textColor = graphics.Color(255, 255, 255)
+    x_pos = 0
+    y_pos = 5
 
-                # Teams & Scores
-                winning_team = self.data['games'][0]['game']['winning_team']['name']
-                winning_team_runs = self.data['games'][0]['game']['winning_team']['runs']
-                losing_team = self.data['games'][0]['game']['losing_team']['name']
-                losing_team_runs = self.data['games'][0]['game']['losing_team']['runs']
 
-                # Pitchers
-                winning_pitcher = self.data['games'][0]['game']['winning_team']['winning_pitcher']['name']
-                winning_pitcher_record_object = self.data['games'][0]['game']['winning_team']['winning_pitcher']['record']
-                winning_pitcher_record = str(winning_pitcher_record_object['wins']) + ' - ' + str(winning_pitcher_record_object['losses'])
+    games = self.data['games']
 
-                losing_pitcher = self.data['games'][0]['game']['losing_team']['losing_pitcher']['name']
-                losing_pitcher_record_object = self.data['games'][0]['game']['losing_team']['losing_pitcher']['record']
-                losing_pitcher_record = str(losing_pitcher_record_object['wins']) + ' - ' + str(losing_pitcher_record_object['losses'])
+    for game in games:
+    
+      # Teams & Scores
+      winning_team = game['game']['winning_team']['name']
+      winning_team_runs = game['game']['winning_team']['runs']
+      losing_team = game['game']['losing_team']['name']
+      losing_team_runs = game['game']['losing_team']['runs']
 
-                while True:
-                        offscreen_canvas.Clear()
-                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos, textColor, winning_team)                                              
-                        graphics.DrawText(offscreen_canvas, font, ( offscreen_canvas.width - ( len(str(winning_team_runs)) * 4 ) ), y_pos, textColor, str(winning_team_runs))
-                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 7, textColor, losing_team)                                              
-                        graphics.DrawText(offscreen_canvas, font, ( offscreen_canvas.width - ( len(str(winning_team_runs)) * 4 ) ), y_pos + 7, textColor, str(losing_team_runs))
+      # Pitchers
+      winning_pitcher = game['game']['winning_team']['winning_pitcher']['name']
+      winning_pitcher_record_object = game['game']['winning_team']['winning_pitcher']['record']
+      # winning_pitcher_record = "(" + str(winning_pitcher_record_object['wins']) + '-' + str(winning_pitcher_record_object['losses']) + ")"
 
-                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 21, textColor, "Pitching:")
-                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 28, textColor, "W " + winning_pitcher + winning_pitcher_record)
-                        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 35, textColor, "L " + losing_pitcher + losing_pitcher_record)
-                        
-                        time.sleep(0.05)
-                        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+      losing_pitcher = game['game']['losing_team']['losing_pitcher']['name']
+      losing_pitcher_record_object = game['game']['losing_team']['losing_pitcher']['record']
+      # losing_pitcher_record = "(" + str(losing_pitcher_record_object['wins']) + '-' + str(losing_pitcher_record_object['losses']) + ")"
+
+      save_pitcher = game['game']['winning_team']['save_pitcher']['name']
+      
+      if save_pitcher != '':
+        save_pitcher_saves = str(game['game']['winning_team']['save_pitcher']['saves'])
+
+      if winning_team == game['game']['played_at']['home_team']:
+        home_team = winning_team
+        away_team = losing_team
+        home_team_runs = winning_team_runs
+        away_team_runs = losing_team_runs
+      else:
+        home_team = losing_team
+        away_team = winning_team
+        home_team_runs = losing_team_runs
+        away_team_runs = winning_team_runs
+
+      away_team_formatted = teams[away_team]['short_name'] + " " + teams[away_team]['name']
+      home_team_formatted = teams[home_team]['short_name'] + " " + teams[home_team]['name']
+
+      win_length = len(winning_pitcher)
+      lose_length = len(losing_pitcher)
+      save_length = len(save_pitcher)
+
+      winning_pitcher_formatted = "W " + winning_pitcher[slice(0,1)] + ". " + winning_pitcher[slice(winning_pitcher.index(' ') + 1, win_length)]
+      losing_pitcher_formatted = "L " + losing_pitcher[slice(0,1)] + ". " + losing_pitcher[slice(losing_pitcher.index(' ') + 1, lose_length)]
+      
+      if save_pitcher != "":
+        save_pitcher_formatted = "S " + save_pitcher[slice(0,1)] + ". " + save_pitcher[slice(save_pitcher.index(' ') + 1, save_length)] 
+
+      timeout = 0
+      
+      while timeout < (15 / 0.05): 
+        offscreen_canvas.Clear()
+        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 1, textColor, away_team_formatted)                                              
+        graphics.DrawText(offscreen_canvas, font, ( offscreen_canvas.width - ( len(str(away_team_runs)) * 4 ) ), y_pos + 1, textColor, str(away_team_runs))
+        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 12, textColor, home_team_formatted)                                              
+        graphics.DrawText(offscreen_canvas, font, ( offscreen_canvas.width - ( len(str(home_team_runs)) * 4 ) ), y_pos + 12, textColor, str(home_team_runs))
+
+        # graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 21, textColor, "Pitching:")
+        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 28, textColor, winning_pitcher_formatted)
+        graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 35, textColor, losing_pitcher_formatted)
+
+        if save_pitcher != '':
+                graphics.DrawText(offscreen_canvas, font, x_pos, y_pos + 42, textColor, save_pitcher_formatted)
+        
+        time.sleep(0.05)
+        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+        timeout += 0.5
+
+    self.run()
 
 # Main function
 if __name__ == "__main__":
-    scoreboard = Scoreboard()
-    if (not scoreboard.process()):
-        scoreboard.print_help()
-	
+  scoreboard = Scoreboard()
+  if (not scoreboard.process()):
+      scoreboard.print_help()
+
---
+++ teams.py
@@ -0,0 +1,123 @@
+teams = {
+  "Arizona Diamondbacks": {
+		"short_name": "ARI",
+		"name": "Diamondbacks"
+	},
+	"Atlanta Braves": {
+		"short_name": "ATL",
+		"name": "Braves"
+	},
+	"Baltimore Orioles": {
+		"short_name": "BAL",
+		"name": "Orioles"
+	},
+	"Boston Red Sox": {
+		"short_name": "BOS",
+		"name": "Red Sox"
+	},
+	"Chicago Cubs": {
+		"short_name": "CHC",
+		"name": "Cubs"
+	},
+	"Chicago White Sox": {
+		"short_name": "CWS",
+		"name": "White Sox"
+	},
+	"Cincinnati Reds": {
+		"short_name": "CIN",
+		"name": "Reds"
+	},
+	"Cleveland Indians": {
+		"short_name": "CLE",
+		"name": "Indians"
+	},
+	"Colorado Rockies": {
+		"short_name": "COL",
+		"name": "Rockies"
+	},        
+	"Detroit Tigers": {
+		"short_name": "DET",
+		"name": "Tigers"
+	},        
+	"Houston Astros": {
+		"short_name": "HOU",
+		"name": "Astros"
+	},        
+	"Kansas City Royals": {
+		"short_name": "KC ",
+		"name": "Royals"
+	},
+	"Los Angeles Angels": { 
+		"short_name":"LAA",
+		"name": "Angels"
+	},
+	"Los Angeles Dodgers": {
+		"short_name": "LAD",
+		"name": "Dodgers"
+	},
+	"Miami Marlins": {
+		"short_name": "MIA",
+		"name": "Marlins"
+	},
+	"Milwaukee Brewers": {
+		"short_name": "MIL",
+		"name": "Brewers"
+	},
+	"Minnesota Twins": {
+		"short_name": "MIN",
+		"name": "Twins"
+	},
+	"New York Mets": {
+		"short_name": "NYM",
+		"name": "Mets"
+	},
+	"New York Yankees": {
+		"short_name": "NYY",
+		"name": "Yankees"
+	},
+	"Oakland Athletics": {
+		"short_name": "OAK",
+		"name": "Athletics"
+	},
+	"Philadelphia Phillies": {
+		"short_name": "PHI",
+		"name": "Phillies"
+	},
+	"Pittsburgh Pirates": {
+		"short_name": "PIT",
+		"name": "Pirates"
+	},
+	"San Diego Padres": {
+		"short_name": "SD ",
+		"name": "Padres"
+	},
+	"San Francisco Giants": {
+		"short_name": "SF ",
+		"name": "Giants"
+	},
+        "Seattle Mariners": {
+                "short_name": "SEA",
+                "name": "Mariners"
+        },
+	"St. Louis Cardinals": {
+		"short_name": "STL",
+		"name": "Cardinals"
+	},
+	"Tampa Bay Rays": {
+		"short_name": "TB ",
+		"name": "Rays"
+	},
+	"Texas Rangers": {
+		"short_name": "TEX",
+		"name": "Rangers"
+	},
+	"Toronto Blue Jays": {
+		"short_name": "TOR",
+		"name": "Blue Jays"
+	},
+	"Washington Nationals": {
+		"short_name": "WSH",
+		"name": "Nationals"
+	}
+}
+

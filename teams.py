8f6b1e5ee6865edd347ceaabdcb54dbe2866fa27
unknown <tyler.b.porter@gmail.com>
2019-06-20 21:02:35 -0500

    Added team colors to display

--- teams.py
+++ teams.py
@@ -1,123 +1,273 @@
 teams = {
   "Arizona Diamondbacks": {
-		"short_name": "ARI",
-		"name": "Diamondbacks"
-	},
-	"Atlanta Braves": {
-		"short_name": "ATL",
-		"name": "Braves"
-	},
-	"Baltimore Orioles": {
-		"short_name": "BAL",
-		"name": "Orioles"
-	},
-	"Boston Red Sox": {
-		"short_name": "BOS",
-		"name": "Red Sox"
-	},
-	"Chicago Cubs": {
-		"short_name": "CHC",
-		"name": "Cubs"
-	},
-	"Chicago White Sox": {
-		"short_name": "CWS",
-		"name": "White Sox"
-	},
-	"Cincinnati Reds": {
-		"short_name": "CIN",
-		"name": "Reds"
-	},
-	"Cleveland Indians": {
-		"short_name": "CLE",
-		"name": "Indians"
-	},
-	"Colorado Rockies": {
-		"short_name": "COL",
-		"name": "Rockies"
-	},        
-	"Detroit Tigers": {
-		"short_name": "DET",
-		"name": "Tigers"
-	},        
-	"Houston Astros": {
-		"short_name": "HOU",
-		"name": "Astros"
-	},        
-	"Kansas City Royals": {
-		"short_name": "KC ",
-		"name": "Royals"
-	},
-	"Los Angeles Angels": { 
-		"short_name":"LAA",
-		"name": "Angels"
-	},
-	"Los Angeles Dodgers": {
-		"short_name": "LAD",
-		"name": "Dodgers"
-	},
-	"Miami Marlins": {
-		"short_name": "MIA",
-		"name": "Marlins"
-	},
-	"Milwaukee Brewers": {
-		"short_name": "MIL",
-		"name": "Brewers"
-	},
-	"Minnesota Twins": {
-		"short_name": "MIN",
-		"name": "Twins"
-	},
-	"New York Mets": {
-		"short_name": "NYM",
-		"name": "Mets"
-	},
-	"New York Yankees": {
-		"short_name": "NYY",
-		"name": "Yankees"
-	},
-	"Oakland Athletics": {
-		"short_name": "OAK",
-		"name": "Athletics"
-	},
-	"Philadelphia Phillies": {
-		"short_name": "PHI",
-		"name": "Phillies"
-	},
-	"Pittsburgh Pirates": {
-		"short_name": "PIT",
-		"name": "Pirates"
-	},
-	"San Diego Padres": {
-		"short_name": "SD ",
-		"name": "Padres"
-	},
-	"San Francisco Giants": {
-		"short_name": "SF ",
-		"name": "Giants"
-	},
-        "Seattle Mariners": {
-                "short_name": "SEA",
-                "name": "Mariners"
-        },
-	"St. Louis Cardinals": {
-		"short_name": "STL",
-		"name": "Cardinals"
-	},
-	"Tampa Bay Rays": {
-		"short_name": "TB ",
-		"name": "Rays"
-	},
-	"Texas Rangers": {
-		"short_name": "TEX",
-		"name": "Rangers"
-	},
-	"Toronto Blue Jays": {
-		"short_name": "TOR",
-		"name": "Blue Jays"
-	},
-	"Washington Nationals": {
-		"short_name": "WSH",
-		"name": "Nationals"
-	}
+    "short_name": "ARI",
+    "name": "D-backs",
+    "color": {
+      "red": 167,
+      "green": 25,
+      "blue": 48
+    }
+  },
+  "Atlanta Braves": {
+    "short_name": "ATL",
+    "name": "Braves",
+    "color": {
+      "red": 19,
+      "green": 39,
+      "blue": 79
+    }
+  },
+  "Baltimore Orioles": {
+    "short_name": "BAL",
+    "name": "Orioles",
+    "color": {
+      "red": 223,
+      "green": 70, 
+      "blue": 1
+    }
+  },
+  "Boston Red Sox": {
+    "short_name": "BOS",
+    "name": "Red Sox",
+    "color": {
+      "red": 189,
+      "green": 48,
+      "blue": 57
+    }
+  },
+  "Chicago Cubs": {
+    "short_name": "CHC",
+    "name": "Cubs",
+    "color": {
+      "red": 14,
+      "green": 51,
+      "blue": 134
+    }
+  },
+  "Chicago White Sox": {
+    "short_name": "CWS",
+    "name": "White Sox",
+    "color": {
+      "red": 39,
+      "green": 37,
+      "blue": 31
+    }
+  },
+  "Cincinnati Reds": {
+    "short_name": "CIN",
+    "name": "Reds",
+    "color": {
+      "red": 198,
+      "green": 1,
+      "blue": 31
+    }
+  },
+  "Cleveland Indians": {
+    "short_name": "CLE",
+    "name": "Indians",
+    "color": {
+      "red": 12,
+      "green": 35,
+      "blue": 64
+    }
+  },
+  "Colorado Rockies": {
+    "short_name": "COL",
+    "name": "Rockies",
+    "color": {
+      "red": 51,
+      "green": 0,
+      "blue": 111
+    }
+  },        
+  "Detroit Tigers": {
+    "short_name": "DET",
+    "name": "Tigers",
+    "color": {
+      "red": 12,
+      "green": 35,
+      "blue": 64
+    }
+  },        
+  "Houston Astros": {
+    "short_name": "HOU",
+    "name": "Astros",
+    "color": {
+      "red": 0,
+      "green": 45,
+      "blue": 98
+    }
+  },        
+  "Kansas City Royals": {
+    "short_name": "KC ",
+    "name": "Royals",
+    "color": {
+      "red": 0,
+      "green": 70,
+      "blue": 135
+    }
+  },
+  "Los Angeles Angels": { 
+    "short_name":"LAA",
+    "name": "Angels",
+    "color": {
+      "red": 186,
+      "green": 0,
+      "blue": 33
+    }
+  },
+  "Los Angeles Dodgers": {
+    "short_name": "LAD",
+    "name": "Dodgers",
+    "color": {
+      "red": 0,
+      "green": 90,
+      "blue": 156
+    }
+  },
+  "Miami Marlins": {
+    "short_name": "MIA",
+    "name": "Marlins",
+    "color": {
+      "red": 0,
+      "green": 0,
+      "blue": 0
+    }
+  },
+  "Milwaukee Brewers": {
+    "short_name": "MIL",
+    "name": "Brewers",
+    "color": {
+      "red": 19,
+      "green": 41,
+      "blue": 75
+    }
+  },
+  "Minnesota Twins": {
+    "short_name": "MIN",
+    "name": "Twins",
+    "color": {
+      "red": 0,
+      "green": 43,
+      "blue": 92
+    }
+  },
+  "New York Mets": {
+    "short_name": "NYM",
+    "name": "Mets",
+    "color": {
+      "red": 0,
+      "green": 45,
+      "blue": 114
+    }
+  },
+  "New York Yankees": {
+    "short_name": "NYY",
+    "name": "Yankees",
+    "color": {
+      "red": 12,
+      "green": 35,
+      "blue": 64
+    }
+  },
+  "Oakland Athletics": {
+    "short_name": "OAK",
+    "name": "Athletics",
+    "color": {
+      "red": 0,
+      "green": 56,
+      "blue": 49
+    }
+  },
+  "Philadelphia Phillies": {
+    "short_name": "PHI",
+    "name": "Phillies",
+    "color": {
+      "red": 232,
+      "green": 24,
+      "blue": 40
+    }
+  },
+  "Pittsburgh Pirates": {
+    "short_name": "PIT",
+    "name": "Pirates",
+    "color": {
+      "red": 39,
+      "green": 37,
+      "blue": 37
+    }
+  },
+  "San Diego Padres": {
+    "short_name": "SD ",
+    "name": "Padres",
+    "color": {
+      "red": 0,
+      "green": 45,
+      "blue": 98
+    }
+  },
+  "San Francisco Giants": {
+    "short_name": "SF ",
+    "name": "Giants",
+    "color": {
+      "red": 39,
+      "green": 37,
+      "blue": 31
+    }
+  },
+  "Seattle Mariners": {
+    "short_name": "SEA",
+    "name": "Mariners",
+    "color": {
+      "red": 12,
+      "green": 44,
+      "blue": 86
+    }
+  },
+  "St. Louis Cardinals": {
+    "short_name": "STL",
+    "name": "Cardinals",
+    "color": {
+      "red": 196,
+      "green": 30,
+      "blue": 58
+    }
+  },
+  "Tampa Bay Rays": {
+    "short_name": "TB ",
+    "name": "Rays",
+    "color": {
+      "red": 9,
+      "green": 44,
+      "blue": 92
+    }
+  },
+  "Texas Rangers": {
+    "short_name": "TEX",
+    "name": "Rangers",
+    "color": {
+      "red": 0,
+      "green": 50,
+      "blue": 120
+    }
+  },
+  "Toronto Blue Jays": {
+    "short_name": "TOR",
+    "name": "Blue Jays",
+    "color": {
+      "red": 19,
+      "green": 74,
+      "blue": 142
+    }
+  },
+  "Washington Nationals": {
+    "short_name": "WSH",
+    "name": "Nationals",
+    "color": {
+      "red": 171,
+      "green": 0,
+      "blue": 3
+    }
+  }
 }
 
